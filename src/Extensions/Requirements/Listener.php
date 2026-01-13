88b664339b22bb04996628a50c50f25293b7bc3e
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2026-01-13 17:28:43 +0400

    feat(phpunit-extensions): New extension `\LastDragon_ru\PhpUnit\Extensions\Requirements\Extension`.

---
+++ src/Extensions/Requirements/Listener.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types = 1);
+
+namespace LastDragon_ru\PhpUnit\Extensions\Requirements;
+
+use PHPUnit\Event\Code\TestMethod;
+use PHPUnit\Event\Event;
+use PHPUnit\Event\Test\BeforeTestMethodCalled;
+use PHPUnit\Event\Test\PreConditionCalled;
+use PHPUnit\Event\Test\Prepared;
+use PHPUnit\Framework\Assert;
+
+use function implode;
+
+use const PHP_EOL;
+
+/**
+ * @internal
+ */
+class Listener {
+    public function __construct(
+        protected readonly Checker $checker,
+    ) {
+        // empty
+    }
+
+    public function notify(Event $event): void {
+        // Supported?
+        [$class, $method] = $this->getTarget($event);
+
+        if ($class === null) {
+            return;
+        }
+
+        // Satisfied?
+        $failed    = [];
+        $satisfied = $this->checker->isSatisfied($class, $method, $failed);
+
+        if (!$satisfied) {
+            Assert::markTestSkipped(implode(PHP_EOL, $failed !== [] ? $failed : ['Unknown requirement.']));
+        }
+    }
+
+    /**
+     * @return array{?class-string, ?string}
+     */
+    protected function getTarget(Event $event): array {
+        $class  = null;
+        $method = null;
+
+        if ($event instanceof BeforeTestMethodCalled || $event instanceof PreConditionCalled) {
+            $class = $event->test()->className();
+        } elseif ($event instanceof Prepared) {
+            $test = $event->test();
+
+            if ($test instanceof TestMethod) {
+                $class  = $test->className();
+                $method = $test->methodName();
+            }
+        } else {
+            // empty
+        }
+
+        return [$class, $method];
+    }
+}
