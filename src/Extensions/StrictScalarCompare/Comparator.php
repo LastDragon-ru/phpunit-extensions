36dc9c900384af8af9e51474fd403ec2e6598606
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2026-02-14 09:45:46 +0000

    release: Improvements and Fixes 💖

--- src/Extensions/StrictScalarCompare/Comparator.php
+++ src/Extensions/StrictScalarCompare/Comparator.php
@@ -5,7 +5,7 @@ namespace LastDragon_ru\PhpUnit\Extensions\StrictScalarCompare;
 use LastDragon_ru\PhpUnit\Extensions\StrictScalarComparator\Comparator as NewComparator;
 
 /**
- * @deprecated %{VERSION} Please use {@see NewComparator} instead.
+ * @deprecated 10.2.0 Please use {@see NewComparator} instead.
  */
 class Comparator extends NewComparator {
     // empty
--- src/Extensions/StrictScalarCompare/Extension.php
+++ src/Extensions/StrictScalarCompare/Extension.php
@@ -5,7 +5,7 @@ namespace LastDragon_ru\PhpUnit\Extensions\StrictScalarCompare;
 use LastDragon_ru\PhpUnit\Extensions\StrictScalarComparator\Extension as NewExtension;
 
 /**
- * @deprecated %{VERSION} Please use {@see NewExtension} instead.
+ * @deprecated 10.2.0 Please use {@see NewExtension} instead.
  */
 class Extension extends NewExtension {
     // empty
